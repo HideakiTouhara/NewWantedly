ddf96d2658caa35e6386bac9f3719caf337c29df
HideakiTouhara <hideee6723@gmail.com>
2018-04-29 19:07:53 +0900

    Create SearchListViewController

---
+++ NewWantedly/SearchListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchListTableViewCell.swift
+//  NewWantedly
+//
+//  Created by HideakiTouhara on 2018/04/29.
+//  Copyright © 2018年 HideakiTouhara. All rights reserved.
+//
+
+import UIKit
+
+class SearchListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
